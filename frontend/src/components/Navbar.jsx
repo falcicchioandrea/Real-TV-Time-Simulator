3f2a61f0484aa171257bc853082698dd54ba811e
zGolden <goldenpvp31@gmail.com>
2026-05-12 17:48:07 +0200

    Aggiunto il logo della navbar
    Creata una prima navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,7 +1,28 @@
 import React from "react";
+import Logo from "../assets/logo.png";
 
-function Navbar() {
-  return <div>Navbar</div>;
-}
+const Navbar = () => {
+  return (
+    <nav className="bg-[#ffd400] text-black flex justify-between items-center p-4 h-20 text-sm md:text-[15px] font-medium text-nowrap">
+      <img
+        src={Logo}
+        alt="Logo"
+        className="w-40 cursor-pointer brightness-125"
+      />
+      <ul className="hidden xl:flex space-x-6">
+        <li>Serie</li>
+        <li>Film</li>
+        <li>Generi</li>
+      </ul>
+      <div>
+        <div>
+          <input type="text" placeholder="Cerca titoli" />
+        </div>
+        <button>Accedi</button>
+        <button>Registrati</button>
+      </div>
+    </nav>
+  );
+};
 
 export default Navbar;
