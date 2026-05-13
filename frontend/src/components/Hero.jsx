f38edcac6ed89fb2568485b9ddac3d84833f6d72
falcicchioandrea <a.falcicchio4@studenti.poliba.it>
2026-05-13 23:44:13 +0200

    Aggiunto pulsante REGISTRATI GRATUITAMENTE

--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -3,8 +3,15 @@ import heroBg from '../assets/herobg_placeholder.png'
 
 const Hero = () => {
   return (
-    <div className="">
-        <img src={heroBg} alt="Hero background" />
+    <div className="relative w-full h-[500px] md:h-[600px]">
+        <img src={heroBg} alt="Hero background" 
+        className="w-full h-full object-cover"/>
+
+        <div className="">
+            <button className="absolute cursor-pointer bottom-20 left-10 bg-black text-white font-bold py-4 px-10 rounded-2xl hover:bg-zinc-800">
+                REGISTRATI GRATUITAMENTE
+            </button>
+        </div>
     </div>
   )
 }
