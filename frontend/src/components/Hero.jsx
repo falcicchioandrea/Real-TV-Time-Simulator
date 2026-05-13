875e291cef8469681edc04e37e169ecb90bc7a8f
falcicchioandrea <a.falcicchio4@studenti.poliba.it>
2026-05-13 22:53:45 +0200

    Aggiunto bg provvisorio Hero Section e pagina Homepage

---
+++ frontend/src/components/Hero.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import heroBg from '../assets/herobg_placeholder.png'
+
+const Hero = () => {
+  return (
+    <div className="">
+        <img src={heroBg} alt="Hero background" />
+    </div>
+  )
+}
+
+export default Hero
