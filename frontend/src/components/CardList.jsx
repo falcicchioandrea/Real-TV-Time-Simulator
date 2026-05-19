c252c88578f62a3cab0a7b19df76ad2fc796cc2f
falcicchioandrea <a.falcicchio4@studenti.poliba.it>
2026-05-19 17:54:51 +0200

    Aggiunto Footer

--- frontend/src/components/CardList.jsx
+++ frontend/src/components/CardList.jsx
@@ -83,7 +83,7 @@ const CardList = () => {
 
             <h2 className="inline-block pb-5 pt-5 font-medium text-xl cursor-pointer">I Film più visti</h2>
 
-            <div className="flex flex-shrink-0 overflow-x-auto scroll-smooth [&::-webkit-scrollbar]:hidden">
+            <div className="flex flex-shrink-0 overflow-x-auto scroll-smooth [&::-webkit-scrollbar]:hidden pb-5">
                 {data.map((item) => (
                     <div className= "flex-shrink-0">
                         <img src={CardImg} alt="" className="h-44 w-full pl-4 pr-4 pt-2 cursor-pointer" />
