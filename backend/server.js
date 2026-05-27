f2223ef52ab9816d29c279d42055e00df1350bb3
falcicchioandrea@gmail.com <falcicchioandrea@gmail.com>
2026-05-27 19:57:32 +0200

    "Creato server sul file server.js"

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,16 @@
 import express from "express"; // Importa il modulo Express: posso farlo solo se ho "type": "module" in package.json
+
+const app = express(); // Crea un'app Express
+const port = 3000; // Definisce la porta su cui il server ascolterà
+
+app.get("/", (req, res) => {
+    res.send("Registrati sulla mia applicazione!"); // Risponde con un messaggio quando viene effettuata una richiesta GET alla radice (root)
+})
+
+app.listen(port, () => {
+    console.log(`Il server è in ascolto sulla porta: ${port}`); // Avvia il server e stampa un messaggio di conferma
+})
+
+
+
+
