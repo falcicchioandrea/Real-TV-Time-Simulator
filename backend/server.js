ae895313a7141e3382908bd0da1e7fbda8c4be71
falcicchioandrea@gmail.com <falcicchioandrea@gmail.com>
2026-05-29 08:40:55 +0200

    "Hash&State della password e userDoc"

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from "express"; // Importa il modulo Express: posso farlo solo se
 import { connectToDB } from "./config/db.js"; // Importa la funzione per connettersi al database
 import dotenv from "dotenv"; // Importa il modulo dotenv per gestire le variabili d'ambiente (utile per MONGO_URI)
 import User from "./models/user.model.js";
+import bcryptjs from "bcryptjs";
 
 dotenv.config(); // Carica le variabili d'ambiente dal file .env
 
@@ -16,9 +17,6 @@ const PORT = process.env.PORT || 5000; // Definisce la porta su cui il server as
 app.get("/", (req, res) => {
     res.send("Registrati sulla mia applicazione!"); // Risponde con un messaggio quando viene effettuata una richiesta GET alla radice (root)
 })
-
-// Inserisco una API per costruire una rotta (endpoint) '/api/registrati' verso cui l'utente farà una post
-// inviando i dati che il server si aspetta (username, password e email)
 
 app.post("/api/registrati", async (req, res) => {
     const { username, email, password } = req.body;
@@ -36,18 +34,39 @@ app.post("/api/registrati", async (req, res) => {
         const usernameExists = await User.findOne({ username })
 
         if(usernameExists){
-            return res.status(400).json({ message: "Esiste già un Utente con questo Username, provane un altro."})
+            return res
+            .status(400)
+            .json({ message: "Esiste già un Utente con questo Username, provane un altro."})
         }
 
         const emailExists = await User.findOne({ email })
 
         if(emailExists){
-            return res.status(400).json({ message: "Esiste già un Utente con questa email."})
+            return res
+            .status(400)
+            .json({ message: "Esiste già un Utente con questa email."})
         }
 
-       
+        // HASH&SALT della Password
+        // Questo meccanismo è un meccanismo di hash&salt che prende in input la password in chiaro dell'utente
+        // e il numero di "salt" (in questo caso 10) che indica quante volte viene applicato l'algoritmo di hashing alla password.
+        // Più alto è il numero di salt, più sicura sarà la password hashata, ma richiederà anche  più tempo per essere calcolata.
+        // 10 o 8 è un numero comune per bilanciare sicurezza e prestazioni. 
 
-        return res.status(200).json({user: "Registrazione andata a buon fine!"});
+        const hashedPassword = await bcryptjs.hash(password, 10);
+
+        // Memorizzazione dati dell'Utente: ATTENZIONE che memorizziamo non la password in chiaro ma quella hashata
+        // Creo uno User Document (come se fosse una carta di identità dell'utente) nel nostro database con le informazioni dell'utente registrato
+        
+        const userDoc = await User.create({
+            username,
+            email,
+            password: hashedPassword,
+        })
+
+        return res
+        .status(200)
+        .json({user: "Registrazione andata a buon fine!"});
         
     } catch(error){
 
