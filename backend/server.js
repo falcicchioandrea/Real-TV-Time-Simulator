7bfa94ba7394710addfb8fb8a12440ab2757a2fe
falcicchioandrea@gmail.com <falcicchioandrea@gmail.com>
2026-05-29 08:23:58 +0200

    "Aggiunta la POST per l'invio dei dati da User"

--- backend/server.js
+++ backend/server.js
@@ -5,16 +5,37 @@ import dotenv from "dotenv"; // Importa il modulo dotenv per gestire le variabil
 dotenv.config(); // Carica le variabili d'ambiente dal file .env
 
 const app = express(); // Crea un'app Express
+
+// Middleware che permette di parsare i dati che l'utente invia in formato JSON
+
+app.use(express.json());
+
 const PORT = process.env.PORT || 5000; // Definisce la porta su cui il server ascolterà
 
 app.get("/", (req, res) => {
     res.send("Registrati sulla mia applicazione!"); // Risponde con un messaggio quando viene effettuata una richiesta GET alla radice (root)
 })
 
+// Inserisco una API per costruire una rotta (endpoint) '/api/registrati' verso cui l'utente farà una post
+// inviando i dati che il server si aspetta (username, password e email)
+
+app.post("/api/registrati", async (req, res) => {
+    const { username, email, password } = req.body;
+
+    // OSS: il campo 'Conferma Password' serve solo per validare la registrazione e non va inserito nello User.Schema e nella POST 
+
+    console.log( username, email, password); // Serve per verificare con PostMan che i dati vengano inviati correttamente
+    
+    return res.status(200).json({user: "Done"}); // Restituisce OK se la registrazione va a buon fine
+});
+
+
 app.listen(PORT, () => {
     connectToDB(); // Connette al database quando il server inizia ad ascoltare
     console.log(`Il server è in ascolto sulla porta: ${PORT}`); // Avvia il server e stampa un messaggio di conferma
 })
+
+
 
 
 
