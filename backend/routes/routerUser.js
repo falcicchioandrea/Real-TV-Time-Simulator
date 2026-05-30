413de937b72ded0855269dcd6d40f6aaf70b26ab
p3pp304 <fuzio.giuseppe2014@gmail.com>
2026-05-30 20:46:57 +0200

    fix error con import e require

--- backend/routes/routerUser.js
+++ backend/routes/routerUser.js
@@ -3,12 +3,9 @@ import express from 'express'  // ALTRO MODO --> const express = require('expres
 
 const router = express.Router();  // creo un router per la gestione delle rotte nella gestione utenti
 
-
 router.post("/registrati", registrati);
 router.post("/accedi", accedi);
 router.get("/fetch-user", fetchUser);
+router.post("/esci", logout);
 
-// POST per il Logout
-router.post("/user-api/esci", logout);
-
- export default router // ALTRO MDOO--> module.exports = router
+ export default router // ALTRO MODO--> module.exports = router con "type": "commonjs"
--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,12 @@
+import dotenv from "dotenv"; // Importa il modulo dotenv per gestire le variabili d'ambiente (utile per MONGO_URI)
 import express from "express"; // Importa il modulo Express: posso farlo solo se ho "type": "module" in package.json
 import { connectToDB } from "./config/db.js"; // Importa la funzione per connettersi al database
-import dotenv from "dotenv"; // Importa il modulo dotenv per gestire le variabili d'ambiente (utile per MONGO_URI)
 import cookieParser from "cookie-parser";
-import userRouter from "./routes/routerUser.js" // ALTRO MODO-->const userRouter = require('./routes/routerUser.js')
 
-dotenv.config(); // Carica le variabili d'ambiente dal file .env
+dotenv.config(); // Carica le variabili d'ambiente dal file .env (DEVONO ESSERE CARICATE PRIMA)
+
+import userRouter from "./routes/routerUser.js" // ALTRO MODO-->const userRouter = require('./routes/routerUser.js') con "type": "commonjs"
+
 const app = express(); // Crea un'app Express
 const PORT = process.env.PORT || 5000; // Definisce la porta su cui il server ascolterà
 
@@ -12,6 +14,7 @@ const PORT = process.env.PORT || 5000; // Definisce la porta su cui il server as
 app.use(express.json());
 app.use(cookieParser());
 
+//TEST
 app.get('/', (req, res) => {
     res.send("Registrati sulla mia applicazione!"); // Risponde con un messaggio quando viene effettuata una richiesta GET alla radice (root)
 });
