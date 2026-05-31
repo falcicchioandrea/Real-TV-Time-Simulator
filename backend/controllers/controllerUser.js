f5291f42b2bb59a9b91982be1f24681daf0a63cb
p3pp304 <fuzio.giuseppe2014@gmail.com>
2026-05-31 17:23:22 +0200

    commento per utilizzo di CORS

--- backend/controllers/controllerUser.js
+++ backend/controllers/controllerUser.js
@@ -2,8 +2,6 @@ import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
 import jwt from "jsonwebtoken";
 
-
-
 export const registrati = async (req, res) => {
     const { username, email, password } = req.body;
 
--- backend/server.js
+++ backend/server.js
@@ -14,9 +14,41 @@ const PORT = process.env.PORT || 5000; // Definisce la porta su cui il server as
 
 // Middlewares
 app.use(cors({
-    origin: "http://localhost:5173",
-    credentials: true,
+// ==============================================================================
+// IL NOSTRO PROBLEMA (La trappola della Same-Origin Policy):
+// ==============================================================================
+// Abbiamo 3 attori in gioco:
+// 1. Il browser (Chrome, Safari, che usa una porta temporanea, es. 54122, per fare materialmente la chiamata).
+// 2. Il frontend React (la cui origine di nascita è http://localhost:5173, ma viene integrato nel browser una volta fatta la prima richiesta al server Vite).
+// 3. Il server Express (il backend in ascolto su http://localhost:5000).
+//
+// POSSIAMO fare la richiesta: la chiamata parte dal browser(Chrome, Safari) e arriva al server Express(porta 5000).
+// Il server Express esegue il suo codice regolarmente e rimanda indietro i dati.
+// IL PROBLEMA: Quando la risposta torna indietro, il browser si accorge che le porte 
+// (5173 del frontend e 5000 del backend) sono diverse. Per sicurezza, il browser applica 
+// la "Same-Origin Policy": intercetta la risposta e si RIFIUTA di consegnare i dati a React.
+
+// ==============================================================================
+// LA SOLUZIONE (CORS come passaporto):
+// ==============================================================================
+// 1. Il server Express applica il middleware CORS. Questo fa sì che il server 
+// inserisca automaticamente un "timbro" nella sua risposta, ovvero l'header: 
+// "Access-Control-Allow-Origin: http://localhost:5173".
+// 
+// 2. Il browser (che agisce da dogana di sicurezza), prima di bloccare tutto, 
+// legge questo header. Vedendolo, capisce che il server Express ha esplicitamente 
+// autorizzato l'origine 5173. 
+//
+// 3. Il browser abbassa le difese e permette all'interfaccia utente (React) 
+// di ricevere, leggere e visualizzare i dati elaborati dal server. 
+    origin: "http://localhost:5173", 
+
+    // 2. Il server inserisce l'header "Access-Control-Allow-Credentials".
+    // Permette lo scambio bidirezionale dei cookie di sessione (sia in lettura -->server può leggere cookie del browser  che in scrittura--> server può allegare cookie e inviarlo al browser) 
+    // tra domini/porte differenti.
+    credentials: true 
 }));
+
 app.use(express.json());
 app.use(cookieParser());
 
