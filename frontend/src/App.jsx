54d94676d6ccdd195c033973761735681dedbb74
p3pp304 <fuzio.giuseppe2014@gmail.com>
2026-05-31 16:49:25 +0200

    integrazione del contesto in App

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,43 +3,24 @@ import Homepage from "./pages/Homepage";
 import LoginModal from "./components/LoginModal";
 import RegisterModal from "./components/RegisterModal"; // Controlla la 'r' interna di RegistrerModal.jsx!
 import React from "react";
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router";
 import Moviepage from "./pages/Moviepage";
 import Footer from "./components/Footer";
 import SearchPage from "./pages/Searchpage"; // 'Searchpage' con la 'p' minuscola per corrispondere al file fisico!
 import UserPage from "./pages/Userpage";
 
 function App() {
-  const [IsLoginOpen, setIsLoginOpen] = React.useState(false);
-  const [IsRegisterOpen, setIsRegisterOpen] = React.useState(false);
 
   return (
     <div>
-      <Navbar
-        onOpenLoginModal={() => setIsLoginOpen(true)}
-        onOpenRegisterModal={() => setIsRegisterOpen(true)}
-      />
-      <LoginModal
-        isOpen={IsLoginOpen} // Controlla se il modal è aperto o chiuso
-        onClose={() => setIsLoginOpen(false)} // Passa la funzione di chiusura del modal al LoginModal
-        onSwitchToRegister={() => {
-          setIsLoginOpen(false); // Chiude il modal di login
-          setIsRegisterOpen(true); // Apre il modal di registrazione
-        }}
-      />
-      <RegisterModal
-        isOpen={IsRegisterOpen} // Controlla se il modal è aperto o chiuso
-        onClose={() => setIsRegisterOpen(false)} // Passa la funzione di chiusura del modal al RegisterModal
-        onSwitchToLogin={() => {
-          setIsRegisterOpen(false); // Chiude il modal di registrazione
-          setIsLoginOpen(true); // Apre il modal di login
-        }}
-      />
+      <Navbar/>
+      <LoginModal/>
+      <RegisterModal/>
       <Routes>
         <Route
           path="/"
           element={
-            <Homepage onOpenRegisterModal={() => setIsRegisterOpen(true)} />
+            <Homepage/>
           }
         />
         <Route path="/movie/:id" element={<Moviepage />} />
