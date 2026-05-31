d5c73cc5b0d17e4717e5b02141b205ddaa192eeb
p3pp304 <fuzio.giuseppe2014@gmail.com>
2026-05-31 16:43:33 +0200

    integrazione del contesto nella Navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,23 +1,24 @@
 import Logo from "../assets/logo.png";
 import { Search, User } from "lucide-react"; // Importa l'icona di ricerca da lucide-react come se fosse un componente React
-import { Link } from "react-router"; // Importa il componente Link da react-router per la navigazione tra le pagine
+import { Link, useNavigate } from "react-router"; // Importa il componente Link da react-router per la navigazione tra le pagine
 import { useState } from "react";
-import { useNavigate } from "react-router-dom";
 import { useAuth } from "../store/authContext.jsx";
 
-const Navbar = ({ onOpenLoginModal, onOpenRegisterModal }) => {
+const Navbar = () => {
   const [searchQuery, setSearchQuery] = useState("");
   const navigate = useNavigate();
+
   const handleSearch = (e) => {
     if (e.key === "Enter" && searchQuery.trim() !== "") {
       // .trim() rimuove gli spazi bianchi all'inizio e alla fine della stringa, assicurandosi che la ricerca non venga eseguita se l'utente ha inserito solo spazi
       navigate(`/search?q=${searchQuery.trim()}`); // Naviga alla pagina di ricerca con la query come parametro nella URL
     }
   };
+
   const resetFormValue = () => setSearchQuery("");
 
   // Estrazione
-  const { user, logout } = useAuth();
+  const { user, logout, openSignup, openLogin} = useAuth();
 
   return (
     <>
@@ -46,8 +47,7 @@ const Navbar = ({ onOpenLoginModal, onOpenRegisterModal }) => {
           </div>
         </div>
         {/* Gruppo destro */}
-        {user ? (
-          // Cosa mostrare se l'utente è loggato
+        {user ? ( // se user esiste (quindi è loggato) mostra:
           <div className="flex items-center space-x-4 relative">
             <Link to={`/profile/${user.username}`}>
               <button className="cursor-pointer px-3 py-1 rounded-md hover:bg-[black] hover:text-white flex items-center gap-2">
@@ -62,19 +62,18 @@ const Navbar = ({ onOpenLoginModal, onOpenRegisterModal }) => {
               Esci
             </button>
           </div>
-        ) : (
-          // Cosa mostrare se l'utente non è loggato
+        ) : ( // se non è loggato mostra:
           <div className="flex items-center space-x-4 relative">
             <button
               className="cursor-pointer px-3 py-1 rounded-md hover:bg-[black] hover:text-white"
-              onClick={() => onOpenLoginModal()}
+              onClick={openLogin}
             >
               Accedi
             </button>
             <span className="text-black/50">|</span>
             <button
               className="cursor-pointer px-3 py-1 rounded-md hover:bg-[black] hover:text-white"
-              onClick={() => onOpenRegisterModal()}
+              onClick={openSignup}
             >
               Registrati
             </button>
