37b63f83bf98a789c0da71862a25e7a796eee2e2
p3pp304 <fuzio.giuseppe2014@gmail.com>
2026-06-02 04:12:42 +0200

    aggiunto background

--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -1,22 +1,28 @@
 import { CheckSquare, Eye, Bell, Search } from "lucide-react";
 import { useAuth } from "../store/authContext";
+import Sfondo from "../assets/sfondo-hero.png"; 
 
 const Hero = () => {
   const {openSignup, user} = useAuth();
   
   return ( //md = medium devices (tablet e desktop), px = padding orizzontale, py = padding verticale, gap = spazio tra elementi in un flex o grid
-    <div className="w-full bg-[#0d0d0d] px-6 py-10 md:px-9 md:h-450]">
-      <div>
+    <div className="w-full px-6 py-10 md:px-9 md:h-450]"
+              style={{
+          backgroundImage: `url(${Sfondo})`,
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+        }}>
+      <div className="text-white">
         {!user ? (
-          <h1 className="font-bold text-white text-3xl md:text-4xl mb-6">
+          <h1 className="font-bold text-3xl md:text-4xl mb-6">
           Unisciti alla più grande community di Film!
           </h1>
         ):(
-          <h1 className="font-bold text-white text-3xl md:text-4xl mb-6">
+          <h1 className="font-bold text-3xl md:text-4xl mb-6">
           Benvenuto, {user.username}!
           </h1>
         )}
-        <div className="text-white flex flex-col gap-3 mb-8 text-sm md:text-base">
+        <div className=" flex flex-col gap-3 mb-8 text-sm md:text-base">
           <p className="flex items-center gap-2">
             <CheckSquare size={20}/> Tieni traccia di tutto quello che guardi
           </p>
