1d142935b25186aca322ddef424f312b6acc99c7
p3pp304 <fuzio.giuseppe2014@gmail.com>
2026-06-11 15:57:00 +0200

    commenti App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,21 +9,25 @@ import Footer from "./components/Footer";
 import SearchPage from "./pages/Searchpage"; // 'Searchpage' con la 'p' minuscola per corrispondere al file fisico!
 import UserPage from "./pages/Userpage";
 
+/* 
+---SPA--> Single Page Application-----
+Quando l'utente naviga tra i diversi link, il browser non ricarica l'intera pagina, 
+ma React Router intercetta l'URL e sostituisce al volo 
+solo il componente centrale all'interno del tag <Routes>
+*/
 function App() {
 
   return (
     <div>
       <Navbar/>
-      <LoginModal/>
+      <LoginModal/>  {/*overlay(sfondo oscurato) modali(finestra)--> pop-up di
+                      login e registrazione attivato da qualunque pagina, controllo centralizzato globalmente da authContext */}
       <RegisterModal/>
-      <Routes>
-        <Route
-          path="/"
-          element={
-            <Homepage/>
-          }
-        />
-        <Route path="/movie/:id" element={<Moviepage />} />
+      <Routes>  {/* contenitore padre Routes contiene i diversi figli Route;
+                  esamina URL corrente del browser e renderizzq componente corrispondente a quel path */}
+        <Route path="/" element={<Homepage/>}/>  {/*Rotta statica con parametri */}
+   {/* quando viene attivata(URL=path) questa specifica rotta, viene montato e mostrato componente  Homapage()   */}  
+        <Route path="/movie/:id" element={<Moviepage />} />  {/*Rotta Dinamica con parametri */}
         <Route path="/search" element={<SearchPage />} />
         <Route path="/profile/:username" element={<UserPage />} />
       </Routes>
